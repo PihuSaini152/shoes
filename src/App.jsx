3a453d87e305ef6c01716f7c4a9174f5b184d096
Sourabh saini <sainisourabh927@gmail.com>
2025-05-13 10:36:12 +0530

    sustainability

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav1 from './Component/Navebar/nav.jsx'
 import NavMain from './Component/Navebar/mainnav.jsx'
 import Footer from './Component/Footer/footer.jsx'
-import Help from './pages/Help/help.jsx'
+// import Help from './pages/Help/help.jsx'
 import Rerun from './pages/Rerun/rerun.jsx'
 import Women from './pages/women/women.jsx'
 import Men from './pages/Men/men.jsx'
-import Home from './pages/Home/Home.jsx'
-import Socks from './pages/Socks/socks.jsx'
+// import Home from './pages/Home/Home.jsx'
+// import Socks from './pages/Socks/socks.jsx'
 
 
 function App() {
@@ -19,12 +19,12 @@ function App() {
     <NavMain/>
 
       <Routes>
-        <Route path='/Home' element={<Home/>}/>
-        <Route path='/Help' element={<Help/>}/>
+        {/* <Route path='/Home' element={<Home/>}/> */}
+        {/* <Route path='/Help' element={<Help/>}/> */}
         <Route path='/Rerun' element={<Rerun/>}/>
         <Route path='/Women' element={<Women/>}/>
         <Route path='/Men' element={<Men/>}/>
-        <Route path='/Socks' element={<Socks/>}/>
+        {/* <Route path='/Socks' element={<Socks/>}/> */}
       </Routes>
     <Footer/>
 
