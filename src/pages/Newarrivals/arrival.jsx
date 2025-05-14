a77f0651d24e449540b5f5f9cf104e90b7b98ce3
Sourabh saini <sainisourabh927@gmail.com>
2025-05-14 10:11:51 +0530

    arrival

--- src/pages/Newarrivals/arrival.jsx
+++ src/pages/Newarrivals/arrival.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './arrival.css'
 
+
 function arrival() {
   return (
     <>
       <div className="arrival-header">
 
+      </div>
+      <div className="arrival-main">
+        <h2>MEN`S ARRIVALS </h2>
+        <h2>WOMEN`S ARRIVALS</h2>
       </div>
     </>
   )
---
+++ src/pages/Newarrivals/women_arrival.jsx
@@ -0,0 +1,14 @@
+
+import './women_arrival.css'
+
+function women_arrival() {
+  return (
+    <>
+      <div className="women-arrival-main">
+        <h1>Shoes</h1>
+      </div>
+    </>
+  )
+}
+
+export default women_arrival
