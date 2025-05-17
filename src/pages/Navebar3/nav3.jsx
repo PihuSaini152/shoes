96e2a860e63f10c2b9efd8bac15d96c9b1613124
PihuSaini152 <sainipihu152@gmail.com>
2025-05-17 11:41:43 +0530

    ok

--- src/pages/Navebar3/nav3.jsx
+++ src/pages/Navebar3/nav3.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Nav3.css';
+import { Link } from 'react-router-dom';
 
 const Nav3 = () => {
   return (
     <nav className="nav3">
-      
       <div className="nav-links">
         <ul>
-          <li><Link to="/Men">Men's Shoes</Link></li>
-          <li><Link to ="./Women">Women's Shoes</Link></li>
-          <li><Link to="/Arrivals">New Arrivals</Link></li>
+          <li><Link to="/Menshoes">Men's Shoes</Link></li>
+          <li><Link to="/Womenshoes">Women's Shoes</Link></li>
+          <li><Link to="/Arrival">New Arrivals</Link></li>
         </ul>
       </div>
     </nav>
